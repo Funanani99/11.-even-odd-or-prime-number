788da501fdead5e9c2e1c01acd29ab7cc140ecbe
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 08:55:14 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Mynumber As Integer
+        Dim isEven As Boolean
+        Dim isOdd As Boolean
+        Mynumber = Val(TextBox1.Text)
+        If Mynumber Mod 2 = 0 Then
+            isEven = True
+            MsgBox("The number " & " " & Mynumber & " is an even number")
+        ElseIf Mynumber Mod 2 <> 0 Or Mynumber Mod 2 = 0 Then
+            isOdd = True
+            MsgBox("The number " & " " & Mynumber & " is an odd number")
+
+        Else
+            MsgBox(Mynumber & " " & "is an Prime number")
+        End If
+
+    End Sub
+End Class
